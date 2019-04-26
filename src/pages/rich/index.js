ff18b65561e509bd991949cc477d1ecbd83d08ba
liming <63484013@qq.com>
2019-04-26 09:29:02 +0800

    '1'

--- src/pages/rich/index.js
+++ src/pages/rich/index.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
-// import axios from './../../jsonp'
 import { Editor } from 'react-draft-wysiwyg'
-import { EditorState, convertFromRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState, convertHTML, convertFromRaw } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-import draftHtml from 'draftjs-to-html'
+import draftToHtml from 'draftjs-to-html'
+import htmlToDraft from 'html-to-draftjs'
 import {  Card, Button, Modal,message} from 'antd'
 import './index.less'
 
 class Rich extends Component {
-  
-  state = {
-    dialogLayout: false,
-    editorState: EditorState.createEmpty(), 
-    contentState: ''
+  constructor(props) {
+    super(props); //第一步，这是必须的
+    //不能调用state
+    this.state = {
+      dialogLayout: false,
+      editorState: EditorState.createEmpty(), //创建一个空的富文本
+      getContentState: ''
+    }
   }
+
   componentWillMount(){
   
   }
@@ -30,42 +34,63 @@ class Rich extends Component {
   }
   onContentState=(contentState)=>{
     this.setState({
-      contentState
+      getContentState: contentState
     })
   }
-  defaultVal = () => {
-    this.setState({
-      editorState: convertFromRaw('dsfsaf')
-    })
+  componentWillReceiveProps(){
+    let defaultVal= '我是富文本编辑器的回显原始值';
+    // 匹配富文本编辑器格式，回显保存的内容
+    const contentBlock = htmlToDraft(defaultVal);
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+      const editorState = EditorState.createWithContent(contentState);
+      this.setState({
+         editorState
+      })
+    }
   }
   getContent=()=>{
-    if(!draftHtml(this.state.contentState)) {
-      message.info('富文本编辑器内暂时无内容')
+    //只有修改后的内容才有效
+    if(!this.state.editorState) {
+      message.info('富文本编辑器内暂时无内容，请修改后再获取新内容')
       return
     }
     this.setState({
       dialogLayout: true
     })
   }
+  handleSubmit=()=>{
+    let val = this.state.editorState
+    if(!val){
+       message.info('富文本编辑器内暂时无内容,无法提交')
+      return
+    }
+    //val.getCurrentContent() 这是一个ContentState对像结构
+    //convertToRaw() 方法 将一个ContentState对象，转换为一个原始的JS结构。
+    //draftToHtml()方法 将原始js格式转换成html字符串；
+    let news = draftToHtml(convertToRaw(val.getCurrentContent()))
+    console.log(news)
+  }
   render(){
-    const { editorState,contentState } = this.state
+    const { editorState, getContentState } = this.state
     return (
       <div>
         <Card title="富文本编辑器">
           <Button type="primary" onClick={this.clearContent}>清空内容</Button>
-          <Button type="primary" style={{marginLeft:20}} onClick={this.getContent}>获取html文本</Button>
+          <Button type="primary" style={{marginLeft:20}} onClick={this.getContent}>获取修改后的html文本</Button>
         </Card>
-        <Card style={{marginTop:-1}}>
+        <Card style={{marginTop:-1}} className="cardWrap">
           <Editor
             editorState={editorState}
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
-            defaultEditorState={this.defaultVal}
             onContentStateChange={this.onContentState}
-            defaultContentState={this.defaultContent}
             onEditorStateChange={this.onEditorStateChange}
           />
+        </Card>
+        <Card>
+          <Button type="primary" onClick={this.handleSubmit}>提交</Button>
         </Card>
         <Modal title="富文本"
           visible={this.state.dialogLayout}
@@ -77,7 +102,7 @@ class Rich extends Component {
           footer={null}
         >
           {
-            draftHtml(this.state.contentState)
+            draftToHtml(this.state.getContentState)
           }
         </Modal>
       </div>
