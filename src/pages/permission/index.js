fbb3e8f7f64270338fcc20d81d3c9c7bee122016
liming <63484013@qq.com>
2019-05-05 15:06:31 +0800

    '1'

--- src/pages/permission/index.js
+++ src/pages/permission/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import axios from './../../jsonp'
 import Until from './../../config/until';
-import { Form, Input, Select, Table, Card, Button, Modal, message, Tree } from 'antd'
+import { Form, Input, Select, Table, Card, Button, Modal, message, Tree, Transfer} from 'antd'
 import menuConfig from './../../config/menuConfig'
 const FormItem = Form.Item
 const Option = Select.Option
@@ -9,7 +9,8 @@ const { TreeNode } = Tree
 class Premission extends Component {
   state = {
     dialogCreate: false,
-    diaolgAuthor: false
+    diaolgAuthor: false,
+    dialogUser: false
   }
   componentWillMount(){
     this.requestList()
@@ -87,15 +88,78 @@ class Premission extends Component {
       if(res.code === 0){
         this.authorValue.props.form.resetFields()
         this.setState({
-          diaolgAuthor: false
+          diaolgAuthor: false,
+          
         })
         this.requestList()
       }
     })
-
+  }
+  //用户授权
+  userAuthor=()=>{
+    let rowItem = this.state.selectItem
+    if (!rowItem) {
+      message.info('请选择一行数据')
+      return
+    }
+    this.renderPostData(rowItem)
+    this.setState({
+      dialogUser: true,
+      itemAuthorInfor: rowItem
+    })
+  }
+  renderPostData=(res) => {
+    console.log(res.key)
+    axios.ajax({
+      url: '/user/authorization',
+      params: {
+        id: res.key
+      }
+    }).then((res)=>{
+      if(res.code === 0){
+        this.renderUserData(res.result)
+      }
+    })
+  }
+  renderUserData=(data)=>{
+    console.log(data);
+    let targetData = []
+    const sourceData = []
+    data.forEach(item=>{
+      if (item.status === 1) {
+        targetData.push(item.user_id) //目标区域只需要key值，是数据源刷选出来的
+      }
+      sourceData.push({
+        key: item.user_id,
+        title: item.user_name,
+      })
+    })
+    this.setState({
+      sourceData: sourceData, //数据源需要完整的数据
+      targetData: targetData  //目标数据是数据源中刷选出来的
+    })
     
   }
-
+  handlerUserAuthor=()=>{
+    let data = {
+      id: this.state.itemAuthorInfor.key,
+      user_id: this.state.targetData
+    }
+    console.log(data);
+    
+    axios.ajax({
+      url: '/role/list',
+      params: {
+        ...data
+      }
+    }).then(res=>{
+      if(res.code === 0){
+        this.setState({
+          dialogUser: false
+        })
+      }
+    })
+  }
   render(){
     const column = [
       {
@@ -162,7 +226,7 @@ class Premission extends Component {
         <Card className="btnUser">
           <Button type="primary" onClick={this.createRole}>创建角色</Button>
           <Button type="primary" onClick={this.setAuthor}>设置权限</Button>
-          <Button type="primary">用户授权</Button>
+          <Button type="primary" onClick={this.userAuthor}>用户授权</Button>
         </Card>
         <div style={{marginTop:-1}}>
           <Table bordered
@@ -212,6 +276,30 @@ class Premission extends Component {
             patchMenuInfo={(checkedKeys)=>{
               this.setState({
                 menuInfo: checkedKeys
+              })
+            }}
+            authorInfor={this.state.itemAuthorInfor}
+          />
+        </Modal>
+        <Modal
+          title="用户授权"
+          width={600}
+          visible={this.state.dialogUser}
+          onCancel={()=>{
+          //  this.authorValue.props.form.resetFields()
+            this.setState({
+              dialogUser: false
+            })
+          }}
+          onOk={this.handlerUserAuthor}  
+        >
+          <UserDialog 
+            wrappedComponentRef={(val)=>{this.userAuthorValue=val}}
+            sourceData={this.state.sourceData} //数据源
+            targetData={this.state.targetData} //目标值
+            patchUserInfo={(checkedKeys)=>{
+              this.setState({
+                targetData: checkedKeys
               })
             }}
             authorInfor={this.state.itemAuthorInfor}
@@ -349,3 +437,67 @@ class AuthorDialog extends Component {
   }
 }
 AuthorDialog = Form.create({})(AuthorDialog)
+
+//用户授权
+class UserDialog extends Component {
+  getRoleName=(state)=>{
+    return {
+      1: '管理员',
+      2: '财务专员',
+      3: '运营人员',
+      4: '总经理',
+      5: '董事长',
+      6: '普通职员',
+    }[state]
+  }
+  handleSelectChange=(targetKeys)=>{
+    this.props.patchUserInfo(targetKeys)
+  }
+  filterOption = (inputValue, option) => option.title.indexOf(inputValue) > -1
+  render(){
+    const authorInfor = this.props.authorInfor
+    const sourceData = this.props.sourceData
+    const targetData = this.props.targetData
+    const { getFieldDecorator } = this.props.form
+    const formItemLayout = {
+      labelCol: {
+        span:3
+      },
+      wrapperCol: {
+        span:20
+      }
+    }
+    return (
+      <div>
+        <Form>
+          <FormItem label="角色名称" {...formItemLayout}>
+            {
+              getFieldDecorator('role_name', {
+                initialValue: this.getRoleName(authorInfor.role_name)
+              })(
+                <Input type="text" placeholder="请输入角色名称" disabled />
+              )
+            }
+          </FormItem>
+          <FormItem label="选择用户" {...formItemLayout}>
+            <Transfer
+              listStyle = {{width: 200, height: 300}}
+              dataSource={sourceData}
+              targetKeys={targetData}
+              titles={['待选用户', '已选用户']}
+              showSearch
+              filterOption={this.filterOption}
+              // selectedKeys={selectedKeys}
+              onChange={this.handleChange}
+              onSelectChange={this.handleSelectChange}
+              onScroll={this.handleScroll}
+              render={item => item.title}
+            />
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
+}
+
+UserDialog = Form.create({})(UserDialog)
